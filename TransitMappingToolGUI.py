7bab7399559df6a6801ca328031b38ca932f46ad
Rob Nickerson <robertnickerson@gmail.com>
2018-01-27 21:13:29 -0400

    Formatting, text entry fields
    Changed some formatting, added text entry fields and a couple more buttons

--- TransitMappingToolGUI.py
+++ TransitMappingToolGUI.py
@@ -1,5 +1,6 @@
 from Tkinter import *
 from tkFileDialog import *
+import os
 
 #TODO add check for OS (OSX, Windows, Linux) for file directory initial path
 
@@ -11,77 +12,139 @@ class TransitMappingToolGUI:
         self.master = master
         master.title("A Simple Transit Mapping Tool")
 
+        if os.name == 'posix':
+            self.OS = "OSX"
+        elif os.name == 'nt':
+            self.OS = "Windows"
+        else:
+            self.OS = "OSX"
+
         self.OSMFilePath = ""
+        self.OSMLabelText = StringVar()
         self.GTFSFilePath = ""
+        self.GTFSLabelText = StringVar()
         self.OTPFilePath = ""
+        self.OTPLabelText = StringVar()
         self.timePeriodStart = 0000
         self.timePeriodEnd = 0000
         self.maxTravelTime = 0
-        self.OS = "OSX"
 
         self.OSMLabel = Label(master, text="Select the map data (OSM file) for your desired service area:")
-        self.OSMLabel.pack()
+        self.OSMLabel.grid(column=0, row=0)
+
+        self.OSMLabelPath = Label(master, textvariable=self.OSMLabelText, fg="blue")
+        self.OSMLabelPath.grid(column=0, row=1)
 
         self.OSMButton = Button(master, text="Select OSM", command=self.selectOSM)
-        self.OSMButton.pack()
+        self.OSMButton.grid(column=1, row=0)
 
         self.GTFSLabel = Label(master, text="Select the GTFS data for your transit system:")
-        self.GTFSLabel.pack()
+        self.GTFSLabel.grid(column=0, row=2)
+        
+        self.GTFSLabelPath = Label(master, textvariable=self.GTFSLabelText, fg="blue")
+        self.GTFSLabelPath.grid(column=0, row=3)
 
         self.GTFSButton = Button(master, text="Select GTFS", command=self.selectGTFS)
-        self.GTFSButton.pack()
+        self.GTFSButton.grid(column=1, row=2)
 
         self.OTPLabel = Label(master, text="Select your OpenTripPlanner .jar file:")
-        self.OTPLabel.pack()
+        self.OTPLabel.grid(column=0, row=5)
+
+        self.OTPLabelPath = Label(master, textvariable=self.OTPLabelText, fg="blue")
+        self.OTPLabelPath.grid(column=0, row=6)
 
         self.OTPButton = Button(master, text="Select jar file", command=self.selectOTP)
-        self.OTPButton.pack()
+        self.OTPButton.grid(column=1, row=5)
 
         self.timeStartLabel = Label(master, text="Enter the start time for your time increment (e.g. 0700)")
-        self.timeStartLabel.pack()
+        self.timeStartLabel.grid(column=0, row=7)
+
+        self.timeStartEntry = Entry(master)
+        self.timeStartEntry.grid(column=0, row=8)
+
+        self.timeStartButton = Button(master, text="Set", command=self.setStart)
+        self.timeStartButton.grid(column=1,row=8)
 
         self.timeEndLabel = Label(master, text="Enter the end time for your time increment (e.g. 0800)")
-        self.timeEndLabel.pack()
+        self.timeEndLabel.grid(column=0, row=9)
+
+        self.timeEndEntry = Entry(master)
+        self.timeEndEntry.grid(column=0, row=10)
+
+        self.timeEndButton = Button(master, text="Set", command=self.setEnd)
+        self.timeEndButton.grid(column=1, row=10)
 
         self.maxTravelTimeLabel = Label(master, text="Enter the maximum travel time in seconds:")
-        self.maxTravelTimeLabel.pack()
+        self.maxTravelTimeLabel.grid(column=0, row=11)
+
+        self.maxTravelTimeEntry = Entry()
+        self.maxTravelTimeEntry.grid(column=0, row=12)
+
+        self.travelTimeButton = Button(master, text="Set", command=self.setTravelTime)
+        self.travelTimeButton.grid(column=1, row=12)
+
+        self.launchOTPButton = Button(master, text="Launch OpenTripPlanner", command=self.launchOTP)
+        self.launchOTPButton.grid(column=0, row=14)
+
+        self.generateGridButton = Button(master, text="Generate Point Grid", command=self.generateGrid)
+        self.generateGridButton.grid(column=1, row=14)
 
         self.helpButton = Button(master, text="Help", command=self.helpMe)
-        self.helpButton.pack()
+        self.helpButton.grid(column=0, row=15)
 
     #Method to choose the OSM filepath
     def selectOSM(self):
         if self.OS == "OSX" or self.OS == "Linux":
             self.OSMFilePath = askopenfilename(initialdir = "/", title = "Select OSM File", filetypes=(("OSM Files", "*.PBF"), ("all files", "*.*")))
-            print(self.OSMFilePath) #temporary print for testing
 
         elif self.OS == "Windows":
             self.OSMFilePath = askopenfilename(initialdir = "C:\\", title = "Select OSM File", filetypes=(("OSM Files", "*.PBF"), ("all files", "*.*")))
-            print(self.OSMFilePath) #temporary print for testing
+            
+        self.OSMLabelText.set(self.OSMFilePath)
 
     #Method to choose the GTFS filepath
     def selectGTFS(self):
         if self.OS == "OSX" or self.OS == "Linux":
             self.GTFSFilePath = askopenfilename(initialdir = "/", title = "Select GTFS File", filetypes=(("GTFS Files", "*.zip"), ("all files", "*.*")))
-            print(self.GTFSFilePath) #temporary print for testing
 
         elif self.OS == "Windows":
             self.GTFSFilePath = askopenfilename(initialdir = "C:\\", title = "Select GTFS File", filetypes=(("GTFS Files", "*.zip"), ("all files", "*.*")))
-            print(self.GTFSFilePath) #temporary print for testing
+
+        self.GTFSLabelText.set(self.GTFSFilePath)
+
 
     #Method to choose OTP.jar filepath
     def selectOTP(self):
         if self.OS == "OSX" or self.OS == "Linux":
             self.OTPFilePath = askopenfilename(initialdir = "/", title = "Select OpenTripPlanner jar", filetypes=(("jar files", "*.jar"), ("all files", "*.*")))
-            print(self.OTPFilePath) #temporary print for testing
             
         elif self.OS == "Windows":
             self.OTPFilePath = askopenfilename(initialdir = "C:\\", title = "Select OpenTripPlanner jar", filetypes=(("jar Files", "*.jar"), ("all files", "*.*")))
-            print(self.OTPFilePath) #temporary print for testing
+
+        self.OTPLabelText.set(self.OTPFilePath)
+
+    def setStart(self):
+        self.timePeriodStart = self.timeStartEntry.get()
+        print(self.timePeriodStart)
+
+    def setEnd(self):
+        self.timePeriodEnd = self.timeEndEntry.get()
+        print(self.timePeriodEnd)
+
+    def setTravelTime(self):
+        self.maxTravelTime = self.maxTravelTimeEntry.get()
+        print(self.maxTravelTime)
+
+    def launchOTP(self):
+        print("This will eventually launch OpenTripPlanner")
+
+    def generateGrid(self):
+        print("This will eventually generate a point grid in QGIS")
+
 
     #Method to launch help page
     def helpMe(self):
-        print("Help")
+        print("This will eventually launch a help menu")
         #TODO add help functionality: button launches new window which displays explanation and instructions
 
 
